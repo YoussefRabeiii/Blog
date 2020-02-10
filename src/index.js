77d99b5ba3e05136eb8cf9cb2e6ebd76477a4095
Youssef Rabei <youssefrabei2003@gmail.com>
2020-02-10 19:42:26 +0200

    change  HashRouter to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 import Home from './pages/Home';
